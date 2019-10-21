61fccb8497f105c0e9683703a464ecfa219230f7
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-10-21 03:58:13 -0600

    100% Centaur tests complete

--- exercises/centaur.js
+++ exercises/centaur.js
@@ -4,12 +4,53 @@ class Centaur {
     this.breed = breed;
     this.cranky = false;
     this.standing = true;
+    this.layingDown = false;
+    this.crankCount = 0;
   }
   shoot() {
-    return 'Twang!!!';
+    if (this.cranky === false && this.layingDown === false) {
+      this.crankCount++;
+      if (this.crankCount === 3) this.cranky = true;
+      return 'Twang!!!'
+    } else {
+      return 'NO!'
+    }
   }
   run() {
-    return 'Clop clop clop clop!!!'
+    if (this.layingDown === false) {
+      this.crankCount++;
+      if (this.crankCount === 3) this.cranky = true;
+      return 'Clop clop clop clop!!!'
+    } else {
+      return 'NO!'
+    }
+  }
+  sleep() {
+    if (this.standing === false) {
+      this.crankCount = 0;
+      this.cranky = false;
+      return 'ZZZZ'
+    } else {
+      return 'NO!'
+    }
+  }
+  layDown() {
+    this.standing = false;
+    this.layingDown = true;
+  }
+  standUp() {
+    this.standing = true;
+    this.layingDown = false;
+  }
+  drinkPotion() {
+    if (this.standing === true && this.cranky === true) {
+      this.crankCount = 0;
+      this.cranky = false;
+    } else if (this.standing === true) {
+      this.cranky = true;
+    } else {
+      return 'Not while I\'m laying down!'
+    }
   }
 };
 
